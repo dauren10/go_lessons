72484675ee540972515d6074f6b020f1151f5634
Dauren Shalabayev <dauren.shalabayev@gmail.com>
2023-08-12 14:18:05 +0600

    Defer

--- 2/2.2.go
+++ 2/2.2.go
@@ -5,7 +5,27 @@ import "fmt"
 func main() {
 	a := 0
 	fmt.Printf("Hello %d", a)
+
 	for a < 100 {
+
+		i := isTest(a)
+		switch i {
+		case 1:
+			fmt.Println("hello")
+		default:
+			fmt.Println("hello")
+		}
+
+		switch {
+		case i == 2:
+			fmt.Println("hello")
+		default:
+			fmt.Println("hello")
+		}
+
+		if i := isTest(a); i == 1 {
+
+		}
 		if a == 50 {
 			fmt.Println("hello")
 		} else {
@@ -16,12 +36,12 @@ func main() {
 
 }
 
-func isTest(a int) (bool, int) {
+func isTest(a int) int {
 	if a == 2 {
-		return true, 1
+		return 1
 	} else if a == 3 {
-		return true, 3
+		return 3
 	}
 
-	return false, 4
+	return 4
 }
---
+++ 2/2.3.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("world")
+	defer fmt.Println("dauren")
+	defer fmt.Println("shalabayev")
+	fmt.Println("Hello")
+}
+
+//после окончания работы метода, можно очищать ресурсы, принцип стека
